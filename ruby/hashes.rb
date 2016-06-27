9cb4f315092f4513ba2285b13ab9a922353bd2be
neelgupta2 <neelgupta2@gmail.com>
2016-06-27 12:22:29 -0600

    Add final code to hashes.rb file. Finished loop to update hash.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -16,19 +16,51 @@
 def client_program
   puts "Hello and welcome to my store. What is your name?"
   $user_information[:client_name] = gets.chomp
-  p $user_information
+  # p $user_information
 
   puts "Nice to meet you, #{$user_information[:client_name]}"
 
+  puts "And how old are you?"
+  $user_information[:age] = gets.to_i
+  # p $user_information
+
   puts "How many children do you have?"
   $user_information[:num_children] = gets.to_i
-  p $user_information
+  # p $user_information
 
   puts "What kind of theme do you like for decor?"
   $user_information[:decor_theme] = gets.chomp
-  p $user_information
+  # p $user_information
 
-  puts "Thank you for answering these questions, below you can find a list of the information you provided us."
+  puts "Thank you for answering these questions, below you can find a list of the information you provided us. If you would like to change any of this, enter which you would like to change. If not, type none!"
+  p $user_information
+  update = gets.chomp
+
+  if update.include?("client_name") == true
+    puts "Re-enter your name."
+    $user_information[:client_name] = gets.chomp
+    p $user_information
+
+  elsif update.include?("decor_theme") == true
+    puts "Re-enter your theme."
+    $user_information[:decor_theme] = gets.chomp
+    p $user_information
+
+  elsif update.include?("age") == true
+    puts "Re-enter your age."
+    $user_information[:age] = gets.chomp 
+    p $user_information 
+
+  elsif update.include?("num_children") == true
+    puts "Re-enter your theme."
+    $user_information[:num_children] = gets.chomp
+    p $user_information
+
+  else 
+    puts "Thank you for using this program."
+    p $user_information
+    
+  end
 
 end
 
@@ -37,7 +69,7 @@ def user_interface
   $user_information = {}
   puts "Welcome to the design store, are you a client or a designer?"
   $user_information[:user] = gets.chomp
-  p $user_information
+  # p $user_information
 
   if $user_information[:user] == "Client" || $user_information[:user] == "client"
     client_program
@@ -46,6 +78,7 @@ def user_interface
   else
     puts "Invalid Input"
   end
+
 
 end
 

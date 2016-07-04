e868d9ca0356c596c6f6ab7d12243cdd01444355
neelgupta2 <neelgupta2@gmail.com>
2016-07-04 13:58:39 -0700

    Add each and map methods to iteration.rb NEXT: RElease 02

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,4 +1,6 @@
-arsenal_fc = ["Alexis Sanchez", "Olivier Giroud", "Theo Walcott", "Aaron Ramsey", "Hector Bellerin", "Laurent Koscielny", "Nacho Monreal", "Takuma Asano"]
+arsenal_fc = ["Alexis Sanchez", "Olivier Giroud", "Theo Walcott", "Aaron Ramsey", "Hector Bellerin", "", "Laurent Koscielny", "Nacho Monreal", "Takuma Asano", ""]
+
+new_arsenal_fc = []
 
 player_position = {
   forward: "Alexis Sanchez",
@@ -6,8 +8,41 @@ player_position = {
   defense: "Nacho Monreal"
 }
 
-p arsenal_fc
+# # RELEASE 1
+# # Original Data
+# puts "Original Data:"
+# p arsenal_fc
+# p new_arsenal_fc
+# p player_position
 
-arsenal_fc.each {}
+# arsenal_fc.each do |player|
+#   new_arsenal_fc << player + " -> NEW"
+# end
 
-p player_position
+# player_position.each do |position, player|
+#   puts "#{player} is a #{position}"
+# end
+
+# arsenal_fc.map! do |player|
+#   # puts player
+#   player.next
+# end
+
+# player_position
+
+# # Modified Data
+# puts "Modified Data:"
+# p arsenal_fc
+# # p new_arsenal_fc
+# p player_position
+
+# RELEASE 2
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+
+
+
+
+
+

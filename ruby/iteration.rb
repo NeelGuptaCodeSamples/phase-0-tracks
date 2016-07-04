53c36e6fabdd31a65c5de453a6c2e8383900c3cc
neelgupta2 <neelgupta2@gmail.com>
2016-07-04 09:20:01 -0700

    Add hash and array with basic data in iteration.rb NEXT: Iterate over array + hash using each & .map! .map

---
+++ ruby/iteration.rb
@@ -0,0 +1,13 @@
+arsenal_fc = ["Alexis Sanchez", "Olivier Giroud", "Theo Walcott", "Aaron Ramsey", "Hector Bellerin", "Laurent Koscielny", "Nacho Monreal", "Takuma Asano"]
+
+player_position = {
+  forward: "Alexis Sanchez",
+  midflied: "Aaron Ramsey",
+  defense: "Nacho Monreal"
+}
+
+p arsenal_fc
+
+arsenal_fc.each {}
+
+p player_position

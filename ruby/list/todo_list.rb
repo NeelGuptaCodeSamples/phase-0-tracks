5bfed55f099b191937a2da58d64bd8c9c973d7ad
neelgupta2 <neelgupta2@gmail.com>
2016-07-11 16:03:36 -0700

    Add working code to todo_list.rb file.

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,2 +1,26 @@
 class TodoList
+  attr_reader :list
+  attr_accessor 
+
+  def initialize(list)
+    @list = list
+  end
+
+  def get_items
+    @list
+  end
+
+  def get_item(i)
+    @list[i]
+  end
+
+  def add_item(new_item)
+    @list << new_item
+  end
+
+  def delete_item(d_item)
+    @list.delete(d_item)
+  end
+
+  
 end

c5b4a8ec27d18c9fceccbd8f77e913b65793d5bb
neelgupta2 <neelgupta2@gmail.com>
2016-07-11 19:42:02 -0700

    Add pseudocode for gps2_2.rb. NEXT: write create_list method

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,35 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # [fill in any steps here]
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: hash?
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: 
+  # add new item to list created in create_list method
+# output: updated list, hash
+
+# Method to remove an item from the list
+# input: item to be removed, quantity optional
+# steps: 
+  # check if item is in the list
+    # if it is not, print error message
+    # restart method (?)
+    # if it is, remove it from the list
+# output: print updated list, hash
+
+# Method to update the quantity of an item
+# input: item name, new quantity
+# steps:
+  # check if item exists in list
+  # if so, update hash with new value
+  # if not, restart method
+# output: updated list, hash
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:

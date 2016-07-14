8c81f202f0fc2bf97f90af7b696fee673a25dade
neelgupta2 <neelgupta2@gmail.com>
2016-07-14 12:04:04 -0700

    Add release 0 and 1 to data_structures.js

---
+++ js/data_structures.js
@@ -0,0 +1,13 @@
+var colors = ["Blue", "Green", "Red", "Purple"];
+
+var horses = ["Ed", "James", "Bill", "Matt"];
+
+colors.push("Yellow");
+
+horses.push("John");
+
+console.log(colors);
+
+console.log(horses);
+
+

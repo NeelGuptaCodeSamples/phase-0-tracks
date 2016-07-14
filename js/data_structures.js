1b4aebbb762259038dfa504d03b485b6d220289a
neelgupta2 <neelgupta2@gmail.com>
2016-07-14 12:53:58 -0700

    Add for loop for keys and values

--- js/data_structures.js
+++ js/data_structures.js
@@ -25,3 +25,41 @@ for (var i = 0; i < horses.length; i++) {
 // console.log(horses);
 
 console.log(horseColors);
+
+
+
+function Car(wheels, year, model) {
+  console.log("New car:", this);
+
+  this.wheels = wheels;
+  this.year = year;
+  this.model = model;
+
+  this.drive = function() { console.log("Now driving..."); };
+
+  console.log("INITIALIZATION OF CAR COMPLETE");
+}
+
+var new_car = new Car(4, 1947, "Mercedes");
+new_car.drive();
+console.log(new_car);
+
+var another_car = new Car(2, 1974, "BMW");
+another_car.drive();
+console.log(another_car);
+
+var cars = [new_car, another_car];
+
+// keys
+// console.log(Object.keys(new_car));
+var keys = Object.keys(new_car);
+
+
+for (var i = 0; i < keys.length; i++) {
+  console.log(new_car[keys[i]]);
+}
+
+for (var i = 0; i < keys.length; i++) {
+  console.log(keys[i]);
+}
+

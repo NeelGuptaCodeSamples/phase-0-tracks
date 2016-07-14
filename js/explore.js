e216e25ebf7c0cb05105ab56d88cc08ef2d7be5a
neelgupta2 <neelgupta2@gmail.com>
2016-07-13 17:58:54 -0700

    Add pseudocode for 7.1

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// write a method that takes a string as a parameter and reverses the string
+// for each letter in a word
+//   take the last letter of the word
+//   remove it from the word
+//   add it to the front of the word
+// repeat until each letter has been moved.
+

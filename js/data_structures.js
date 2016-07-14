764e457c415fb32e0f0f6a9a988bae3d1c5aa3ba
neelgupta2 <neelgupta2@gmail.com>
2016-07-14 12:20:36 -0700

    Add actual code for release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,4 +10,18 @@ console.log(colors);
 
 console.log(horses);
 
+// Use two arrays to create an object
+// Key is horse name
+// value is color
 
+var horseColors = {};
+
+for (var i = 0; i < horses.length; i++) {
+  horseColors[horses[i]] = colors[i];
+}
+
+// console.log(colors);
+
+// console.log(horses);
+
+console.log(horseColors);
